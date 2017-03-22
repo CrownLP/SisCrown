760ae1486809cad402b8b597a9ee20ed446d3c8c
Samuel Canaviri <samuel.canaviri.r@gmail.com>
2017-03-22 11:08:42 -0400

    Prueba de multiples archivos

--- inventario/urls.py
+++ inventario/urls.py
@@ -9,7 +9,7 @@ from .views import (
 
 urlpatterns = [
 
-    url(r'^instancia/', InstanciaList.as_view(), name='list'),
+    url(r'^instanciaa/', InstanciaList.as_view(), name='list'),
     url(r'^instancia(?P<pk>\d+)$', InstanciaDetail.as_view(), name='detail'),
 
 
