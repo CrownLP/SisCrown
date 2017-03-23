0f3a120d3864218c5e04c1d35032c7cea1223ca5
Samuel Canaviri Roque <samuel.canaviri.r@gmail.com>
2017-03-23 19:34:59 -0400

    Vistas por Clases Lista y Detalles

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,16 +1,45 @@
 from django.conf.urls import url
 
 from .views import (
-    InstanciaList,
+    #Vistas de Listado
+    AccesorioList,
     CelularList,
+    CpuList,
+    InstanciaList,
+    LaptopList,
+    Linea_IPList,
+    MonitorList,
+    Telefono_IPList,
+    #Vistas de Detalle
+    AccesorioDetail,
+    CelularDetail,
+    CpuDetail,
     InstanciaDetail,
-    CelularDetail
+    LaptopDetail,
+    Linea_IPDetail,
+    MonitorDetail,
+    Telefono_IPDetail
+
 )
 
 urlpatterns = [
 
-    url(r'^instancia/', InstanciaList.as_view(), name='list'),
-    url(r'^instancia(?P<pk>\d+)$', InstanciaDetail.as_view(), name='detail'),
+    url(r'^accesorio/', AccesorioList.as_view(), name='accesoriolist'),
+    url(r'^celular/', CelularList.as_view(), name='celularlist'),
+    url(r'^cpu/', CpuList.as_view(), name='cpulist'),
+    url(r'^instancia/', InstanciaList.as_view(), name='instancialist'),
+    url(r'^laptop/', LaptopList.as_view(), name='laptoplist'),
+    url(r'^lineaip/', Linea_IPList.as_view(), name='lineaiplist'),
+    url(r'^monitor/', MonitorList.as_view(), name='monitorlist'),
+    url(r'^telefonoip/', Telefono_IPList.as_view(), name='telefonoiplist'),
 
+    url(r'^item(?P<pk>\d+)$', AccesorioDetail.as_view(), name='accesoriodetail'),
+    url(r'^item(?P<pk>\d+)$', CelularDetail.as_view(), name='celulardetail'),
+    url(r'^item(?P<pk>\d+)$', CpuDetail.as_view(), name='cpudetail'),
+    url(r'^item(?P<pk>\d+)$', InstanciaDetail.as_view(), name='instanciadetail'),
+    url(r'^item(?P<pk>\d+)$', LaptopDetail.as_view(), name='laptopdetail'),
+    url(r'^item(?P<pk>\d+)$', Linea_IPDetail.as_view(), name='lineaipdetail'),
+    url(r'^item(?P<pk>\d+)$', MonitorDetail.as_view(), name='monitordetail'),
+    url(r'^item(?P<pk>\d+)$', Telefono_IPDetail.as_view(), name='telefonoipdetail'),
 
 ]
--- inventario/views.py
+++ inventario/views.py
@@ -1,19 +1,45 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
-
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.core.urlresolvers import reverse_lazy
-from .models import Instancia, Celular
-# Las Vistas de la Aplicacion
+from .models import Instancia, Celular, Accesorio, CPU, Laptop, Linea_IP, Telefono_IP, Monitor
 
-class InstanciaList(ListView):
-    model = Instancia
+# Las Vistas de Lsiatdo
+
+class AccesorioList(ListView):
+    model = Accesorio
 class CelularList(ListView):
     model = Celular
-
-
-class InstanciaDetail(DetailView):
+class CpuList(ListView):
+    model = CPU
+class InstanciaList(ListView):
     model = Instancia
+class LaptopList(ListView):
+    model = Laptop
+class Linea_IPList(ListView):
+    model = Linea_IP
+class MonitorList(ListView):
+    model = Monitor
+class Telefono_IPList(ListView):
+    model = Telefono_IP
+
+
+
+
+class AccesorioDetail(DetailView):
+    model = Accesorio
 class CelularDetail(DetailView):
     model = Celular
+class CpuDetail(DetailView):
+    model = CPU
+class InstanciaDetail(DetailView):
+    model = Instancia
+class LaptopDetail(DetailView):
+    model = Laptop
+class Linea_IPDetail(DetailView):
+    model = Linea_IP
+class MonitorDetail(DetailView):
+    model = Monitor
+class Telefono_IPDetail(DetailView):
+    model = Telefono_IP
